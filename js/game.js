5bee3dfaf3f90e9ead4a191a26100675036eac61
Corina <corina@fastmail.com>
2016-02-23 11:03:59 +0100

    Fix validation

--- js/game.js
+++ js/game.js
@@ -1,17 +1,18 @@
 document.getElementById("circle1").onclick=function() {
   document.getElementById("circle1").style.backgroundColor="transparent";
-}
+};
 
 document.getElementById("circle2").onclick=function() {
   document.getElementById("circle2").style.backgroundColor="transparent";
-}
+};
 
 document.getElementById("circle3").onclick=function() {
   document.getElementById("circle3").style.backgroundColor="transparent";
-}
+};
+
 document.getElementById("circle4").onclick=function() {
   document.getElementById("circle4").style.backgroundColor="transparent";
-}
+};
 
  setTimeout(function(){
    if (document.getElementById("circle1").style.backgroundColor=="transparent" &&
